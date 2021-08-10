3d75d27e7b73b6118d4032c077b69cbe349c4254
Pia Ruusukivi <ruusukivi@gmail.com>
2021-08-10 21:18:00 +0300

    11.21 cypress, more lint fixes

---
+++ cypress/integration/tests.js
@@ -0,0 +1,8 @@
+describe('App', function() {
+    it('front apage can be opened', function() {
+      cy.visit('http://localhost:8000') 
+      cy.contains('Welcome')
+      cy.contains('Messages').click
+      cy.contains('Send!')
+    })
+  })
